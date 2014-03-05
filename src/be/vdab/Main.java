e30c1a08c8486766177f313d2eda34f711a93863
katjaradelicki <katjaradelicki@gmail.com>
2014-03-05 14:25:31 +0100

    Update Main.java
    zo goed als

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo bijna allemaal");
+		System.out.println("Hallo zo goed als allemaal");
 
 	}
 
